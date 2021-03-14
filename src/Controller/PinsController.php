389c35a062a5f595f1dc7ae64009840aeb6db3af
seunguk <seunguk.yun@etu.univ-lyon1.fr>
2021-03-14 20:18:56 +0100

    Add show pages for pins

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
+use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\PinRepository;
+
 
 class PinsController extends AbstractController
 {
@@ -17,5 +19,12 @@ class PinsController extends AbstractController
         $pins = $pinRepository->findAll();
          
         return $this->render('pins/index.html.twig', compact('pins'));
+    }
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     */
+    public function show(Pin $pin) : Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
